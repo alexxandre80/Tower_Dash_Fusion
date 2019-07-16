0f26c496509afbb53d1beba8ea3c5be8d3691112
Alexandre <mandra80080@gmail.com>
2019-07-16 03:16:46 +0200

    Modif visuel + Bouton QUIT
    Ajout de modifs visuel pour les boutons
    Ajout Menu pour le menu principal

---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsScript : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    
+    public void Setvolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
